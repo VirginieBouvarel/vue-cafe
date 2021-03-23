e6c917e75804e58572bbab7aaeb17525c435e7af
Virginie <vbouvarel@lilo.org>
2021-03-23 13:34:17 +0100

    Ajout HomeLink

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <HomeLink/>
+      <a href="/about">A propos</a>
+      <a href="/contact">Contact</a>
+    </nav>
+    <p>Bienvenue sur notre page d'<HomeLink/></p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeLink from './components/HomeLink.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeLink
   }
 }
 </script>
---
+++ src/components/HomeLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <a href="/">Accueil</a>
+</template>
+
+<script>
+export default {
+    name: 'HomeLink'
+}
+</script>
+
+<style>
+a {
+    text-decoration:none;
+}
+</style>
