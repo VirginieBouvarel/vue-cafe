807aa607104f30d51f62c7cfdafc15ec373f718d
Virginie <vbouvarel@lilo.org>
2021-06-14 16:17:59 +0200

    empty

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>  |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/contact">Contact</router-link>
     </div>
     <router-view></router-view>
-	</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
